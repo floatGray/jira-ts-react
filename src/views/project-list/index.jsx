8adb63bc169f066ec6e1e6ed10951df87a2b6961
floatgray <599316783@qq.com>
2023-02-24 13:12:56 +0800

    debounce

--- src/views/project-list/index.jsx
+++ src/views/project-list/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { SearchPanel } from './search-panel'
 import { List } from './list'
 import { useState, useEffect } from 'react'
-import { cleanObject } from '@/utils'
+import { cleanObject, useDebounce, useMount } from '@/utils'
 import * as qs from 'qs'
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -13,24 +13,26 @@ export const ProjectListView = () => {
     name: '',
     personId: ''
   })
+  const debouncedParam = useDebounce(param, 2000)
   const [list, setList] = useState([])
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json())
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(
+      async (response) => {
+        if (response.ok) {
+          setList(await response.json())
+        }
       }
-    })
-  }, [param])
+    )
+  }, [debouncedParam])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [param])
-
+  })
   return (
     <div>
       <SearchPanel users={users} param={param} setParam={setParam} />
