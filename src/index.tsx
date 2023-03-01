cedc26e6928f8c3b93bf19b729fd0b7264703168
floatgray <599316783@qq.com>
2023-03-01 12:03:06 +0800

    add dev tool

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
 import 'normalize.css'
 import './assets/css/index.less'
+import { loadDevTools } from 'jira-dev-tool'
 
 import App from '@/App'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+loadDevTools(() => {
+  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-)
+  root.render(<App />)
+})
