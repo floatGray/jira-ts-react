fdfdd6f4a48f283828264b070b2a4527f7eaf66c
floatgray <599316783@qq.com>
2023-03-02 10:52:14 +0800

    Row component

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,13 +1,43 @@
+import styled from '@emotion/styled'
 import React from 'react'
+import { Row } from './components/lib'
 import { useAuth } from './context/auth-context'
 import { ProjectListView } from './views/project-list'
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth()
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListView />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <HeaderItem>logo</HeaderItem>
+          <HeaderItem>项目</HeaderItem>
+          <HeaderItem>用户</HeaderItem>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListView />
+      </Main>
+    </Container>
   )
 }
+
+const HeaderItem = styled.h3`
+  margin-right: 3rem;
+`
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  height: 100vh;
+`
+
+const Header = styled(Row)`
+  justify-content: space-between;
+`
+const HeaderLeft = styled(Row)``
+const HeaderRight = styled.div``
+const Main = styled.main``
